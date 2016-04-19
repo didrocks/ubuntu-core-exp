2b5216a2729a6628b5d3bfcc7314c9bfa7736619
Didier Roche <didrocks@ubuntu.com>
2016-04-19 08:00:37 +0200

    Add a Singleton helper

--- sdc-demo/homedemo/tools.py
+++ sdc-demo/homedemo/tools.py
@@ -26,6 +26,16 @@ _mainloop = None
 logger = logging.getLogger(__name__)
 
 
+class Singleton(type):
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
 def get_mainloop():
     """Return mainloop object"""
     return _mainloop
