a36f7d3fb389a57494673fcde48157cf5bc7248d
Didier Roche <didrocks@ubuntu.com>
2016-04-19 17:07:01 +0200

    Add calibration slider

--- sdc-demo/homedemo/www/scripts/app.js
+++ sdc-demo/homedemo/www/scripts/app.js
@@ -3,7 +3,8 @@
 
   var app = document.querySelector('#app');
   app.is_calibrating = false;
-  app.calibrationMessage = 'Start calibration';
+  app.calibrationPos = 0;
+  var _lastCalibrationPosSent = 0;
 
   // imports are loaded and elements have been registered
   window.addEventListener('WebComponentsReady', function () {
@@ -15,9 +16,32 @@
       if (calibrationButton.active) {
         app.calibrationMessage = 'End calibration';
       } else {
+        // reset new 0 as 0
+        app.calibrationPos = 0;
+        _lastCalibrationPosSent = 0;
         app.calibrationMessage = 'Start calibration';
-      }
     }
+
+    var calibrationSlider = document.querySelector('#calibrationSlider');
+    calibrationSlider.addEventListener('immediate-value-change', valueChanged);
+    calibrationSlider.addEventListener('value-change', valueChanged);
+
+    function valueChanged() {
+      // we just ended calibration and reset pos to 0
+      if (!calibrationButton.active && app.calibrationPos === 0) {
+        return;
+      }
+      // ensure we are in calibration mode
+      if (!app.is_calibrating) {
+        app.is_calibrating = true;
+        calibrationToggle();
+      }
+
+      // send position over
+      // calibrationSlider.immediateValue - _lastCalibrationPosSent
+      _lastCalibrationPosSent = calibrationSlider.immediateValue;
+    }
+
 
     // only here get the websocket status back and toggle values if needed
     var socket = new WebSocket("ws://" + window.location.hostname + ":8002/");
