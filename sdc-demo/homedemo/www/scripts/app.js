a44e9916a7f7d1a90053980e2b67508f72974edd
Didier Roche <didrocks@ubuntu.com>
2016-04-20 07:39:53 +0200

    Add manual reset button UI and disable other UI when calibrating

--- sdc-demo/homedemo/www/scripts/app.js
+++ sdc-demo/homedemo/www/scripts/app.js
@@ -6,11 +6,13 @@
   app.calibrationPos = 0;
   var _lastCalibrationPosSent = 0;
   app.current_room = 'space';
-  app.availables_room = ['Room1', 'Room2', 'Room3', 'Room1', 'Room2', 'Room3'];
+  app.availables_room = ['earth', 'space', 'gallifrey', 'skaro'];
+  app.current_room_index = 0;
 
   // imports are loaded and elements have been registered
   window.addEventListener('WebComponentsReady', function () {
 
+    /* calibration button */
     var calibrationButton = document.querySelector('#calibrationButton');
     calibrationButton.addEventListener('click', calibrationToggle);
     function calibrationToggle(_) {
@@ -28,6 +30,7 @@
       }
     }
 
+    /* calibration slider */
     var calibrationSlider = document.querySelector('#calibrationSlider');
     calibrationSlider.addEventListener('immediate-value-change', valueChanged);
     calibrationSlider.addEventListener('value-change', valueChanged);
@@ -37,6 +40,7 @@
       if (!calibrationButton.active && app.calibrationPos === 0) {
         return;
       }
+
       // ensure we are in calibration mode
       if (!app.is_calibrating) {
         app.is_calibrating = true;
@@ -48,13 +52,38 @@
       _lastCalibrationPosSent = calibrationSlider.immediateValue;
     }
 
+    /* move current location */
+    var moveSpheroButton = document.querySelector('#move-sphero');
+    moveSpheroButton.addEventListener('iron-select', function () {
+      // don't do anything for noop (can be triggered when external elements change it)
+      if (app.current_room === moveSpheroButton.selectedItemLabel) {
+        return;
+      }
+
+      // TODO: send sphero move message
+    });
+
+    /* manually set current location */
+    var manualResetSpheroButton = document.querySelector('#manualreset-sphero');
+    manualResetSpheroButton.addEventListener('iron-select', function () {
+      // don't do anything for noop (can be triggered when external elements change it)
+      if (app.current_room === manualResetSpheroButton.selectedItemLabel) {
+        return;
+      }
+
+      // TODO: send sphero manual set message
+    });
 
     // only here get the websocket status back and toggle values if needed
     //var socket = new WebSocket("ws://" + window.location.hostname + ":8002/");
 
+    // TODO: receive message from client (and below should be in client callback)
 
     // initiate calibration toggle label
     calibrationToggle();
+
+    // select current sphero position
+    app.current_room_index = app.availables_room.indexOf(app.current_room);
 
   });
 
